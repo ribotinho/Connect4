97224a65b7f34408f25b651c1ef6bdf925a631ae
ribotinho <ribotinho@gmail.com>
2022-10-13 15:25:12 +0200

    Initial Commit

---
+++ Connect4/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Connect4
+//
+//  Created by Pau Ribot Pujolras on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
